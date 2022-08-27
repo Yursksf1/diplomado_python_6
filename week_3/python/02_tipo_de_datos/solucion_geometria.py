6f75ef0daa089e7d1b01ad0486b6b262c0bff994
yurley <yurs.ksf1@gmail.com>
2022-08-27 10:17:37 -0500

    ejemplo aritmetica

---
+++ week_3/python/02_tipo_de_datos/solucion_geometria.py
@@ -0,0 +1,22 @@
+# Rectangulo
+base = 5
+altura = 3
+ancho = 2
+print('dimensiones: base: {}, altura: {}, ancho: {}'.format(base, altura, ancho))
+
+# perimetro
+print('calcluando el perimetro...')
+perimetro = 2 * base + 2 * altura
+print('el perimetro es de: {}'.format(perimetro))
+
+
+# Area
+print('calcluando el area...')
+area = base * altura
+print('el area es de: {}'.format(area))
+
+
+# Volumen
+print('calcluando el volumen...')
+volumen = base * altura * ancho
+print('el volumen es de: {}'.format(volumen))
