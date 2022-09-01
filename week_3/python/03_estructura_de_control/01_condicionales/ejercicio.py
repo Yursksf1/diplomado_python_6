b0ed97643df23c0fc2f0509af7d1f72f4ae04d16
yurley <yurs.ksf1@gmail.com>
2022-08-31 19:34:30 -0500

    define ejericicio

---
+++ week_3/python/03_estructura_de_control/01_condicionales/ejercicio.py
@@ -0,0 +1,30 @@
+'''
+definir un programa en python que pregunte
+tu edad y nos diga si eres o no mayor de edad
+'''
+
+respuesta = int(input('cual es tu edad? '))
+if respuesta >= 18:
+    print('Eres mayor de edad')
+else:
+    print('No eres mayor de edad')
+
+
+'''
+definir un programa en python que reciba una mensaje de texto 
+y alerte al usario si dentro de esa cadena esta la palabra 'amor'
+'''
+
+
+'''
+definir un programa en python que reciba señales (verdad o falso)
+P y Q 
+y evaluen las siguiente expreciones de logica.
+
+P y Q 
+P o Q 
+no P y Q 
+no P
+(P o Q) y (no p)  
+'''
+
