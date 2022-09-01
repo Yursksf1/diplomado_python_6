d70af90ff5f9d09d2e2f8308792567a77491d8ae
yurley <yurs.ksf1@gmail.com>
2022-08-31 20:39:55 -0500

    solution ej 3

--- week_3/python/03_estructura_de_control/01_condicionales/ejercicio.py
+++ week_3/python/03_estructura_de_control/01_condicionales/ejercicio.py
@@ -3,11 +3,11 @@ definir un programa en python que pregunte
 tu edad y nos diga si eres o no mayor de edad
 '''
 
-respuesta = int(input('cual es tu edad? '))
-if respuesta >= 18:
-    print('Eres mayor de edad')
-else:
-    print('No eres mayor de edad')
+# respuesta = int(input('cual es tu edad? '))
+# if respuesta >= 18:
+#     print('Eres mayor de edad')
+# else:
+#     print('No eres mayor de edad')
 
 
 '''
@@ -27,4 +27,12 @@ no P y Q
 no P
 (P o Q) y (no p)  
 '''
+p = int(input("ingresa 1 para true o 0 para false: ")) == 1
+q = int(input("ingresa 1 para true o 0 para false: ")) == 1
+
+print('p', p)
+print('q', q)
+
+print('P y Q es: {}'.format(p and q))
+print('P o Q es: {}'.format(p or q))
 
