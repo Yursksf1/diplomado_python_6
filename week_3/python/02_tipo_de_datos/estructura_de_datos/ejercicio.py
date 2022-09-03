50d0bd9e2c12d2790d692bfd218d67f8be1fa053
yurley <yurs.ksf1@gmail.com>
2022-09-02 20:33:42 -0500

    definr list, dict and tupla mas ejercicio v2

--- week_3/python/02_tipo_de_datos/estructura_de_datos/ejercicio.py
+++ week_3/python/02_tipo_de_datos/estructura_de_datos/ejercicio.py
@@ -13,3 +13,10 @@ print('mis amigos se llaman: {}'.format(amigos))
 
 for amigo in amigos:
     print('tengo un amigo que se llama: {}'.format(amigo))
+
+
+# definir una lista con los nombre de mascotas, el usuario lo ingresara por un input.
+# esta lista debe ser de minimo 5 elementos
+# una vez llena la lista, el programa debe imprimir el elemento numero 3
+# y decir el nombre de dicha mascota y que longitud tiene esa palabra.
+# iprimir si esa logintu es par.
