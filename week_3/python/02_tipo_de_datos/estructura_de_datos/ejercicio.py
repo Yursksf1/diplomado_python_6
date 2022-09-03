40705d6458346ff06cd51a59e1699e9e54a6fe75
yurley <yurs.ksf1@gmail.com>
2022-09-02 20:18:55 -0500

    definr list, dict and tupla mas ejercicio

---
+++ week_3/python/02_tipo_de_datos/estructura_de_datos/ejercicio.py
@@ -0,0 +1,15 @@
+# listas:
+# definir una lista en python que contenga el nombre de 3 amigos,
+# definir una lista en python que contenga el nombre de 3 vecinos,
+# definir una lista en python que contenga la edades de 3 familiares
+
+# 1)
+amigos = [
+    'cristina',
+    'rodrigo',
+    'andres'
+]
+print('mis amigos se llaman: {}'.format(amigos))
+
+for amigo in amigos:
+    print('tengo un amigo que se llama: {}'.format(amigo))
