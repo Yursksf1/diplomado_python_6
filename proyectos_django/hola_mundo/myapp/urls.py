9c8888c52ee1ef5dcd3431dadd18837963617afb
yurley <yurs.ksf1@gmail.com>
2022-09-16 19:15:05 -0500

    add application banckenrolment

--- proyectos_django/hola_mundo/myapp/urls.py
+++ proyectos_django/hola_mundo/myapp/urls.py
@@ -8,6 +8,11 @@ from myapp import views
 
 app_name = 'myapp'
 urlpatterns = [
-    path('', views.index, name="home"),
-    path('vista_uno', views.vista_uno, name="vista_uno")
+    path('', views.sin_ruta, name="sin_ruta"),
+    path('vista_con_texto_v54678', views.vista_con_texto, name="vista_con_texto"),
+    path('vista_con_template', views.vista_con_template, name="vista_con_template"),
+    path('vista_generica', TemplateView.as_view(template_name='index.html'), name='vista_generica'),
+
+    path('home', TemplateView.as_view(template_name='home.html'))
+
 ]
--- proyectos_django/hola_mundo/myapp/views.py
+++ proyectos_django/hola_mundo/myapp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def index(request):
-    print('estoy llamando al index')
-    return HttpResponse('<b> hola mundo desde django <b>')
+def sin_ruta(request):
+    print('>>> sin_ruta')
+    return HttpResponse('<b style="color: blue "> hola mundo desde django esta es una vista sin ruta definida <b>')
 
-def vista_uno(request):
-    return HttpResponse('<h1> hola mundo desde la vista numero uno </h1> <p>esta es una vista nueva </p>')
+def vista_con_texto(request):
+    print('>>> vista_con_texto')
+    return HttpResponse('<h1> hola mundo desde la vista, estoy renderisando este mensaje con texto en python no estoy usando un archivo html </p>')
+
+def vista_con_template(request):
+    print('>>> vista_con_template')
+    template_name = 'vista_desde_views.html'
+    return render(request, template_name=template_name)
