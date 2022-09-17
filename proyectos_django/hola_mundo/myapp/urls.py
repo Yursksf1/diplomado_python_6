46341f191692d75d00942ffd820b2f58b7879e88
yurley <yurs.ksf1@gmail.com>
2022-09-16 21:29:21 -0500

    use fomrs

--- proyectos_django/hola_mundo/myapp/urls.py
+++ proyectos_django/hola_mundo/myapp/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('vista_con_template', views.vista_con_template, name="vista_con_template"),
     path('vista_generica', TemplateView.as_view(template_name='index.html'), name='vista_generica'),
 
-    path('home', TemplateView.as_view(template_name='home.html'))
+    path('home', TemplateView.as_view(template_name='home.html')),
+    path('ejemplo_numeros', views.ejemplo_numeros)
 
 ]
--- proyectos_django/hola_mundo/myapp/views.py
+++ proyectos_django/hola_mundo/myapp/views.py
@@ -13,3 +13,14 @@ def vista_con_template(request):
     print('>>> vista_con_template')
     template_name = 'vista_desde_views.html'
     return render(request, template_name=template_name)
+
+def ejemplo_numeros(request):
+    template_name = 'ejemplo_numeros.html'
+    number_1 = int(request.GET.get('number_1'))
+    number_2 = int(request.GET.get('number_2'))
+    suma = number_1 + number_2
+
+    context = {
+        "result": suma
+    }
+    return render(request, template_name=template_name, context=context)
